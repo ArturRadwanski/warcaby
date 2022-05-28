54b878b93a2c18aa2453e50c4a9d03a1adbb3fa2
ArturRadwanski <Artur.radw@wp.pl>
2022-05-28 23:07:36 +0200

    style

--- static/Ui.js
+++ static/Ui.js
@@ -18,9 +18,11 @@ class Ui {
         this.game.setCameraView(res.player); //ustawia kamerę z odpowiedniej strony
         document.getElementById("wait").style.display = "none"; //ukrywa div z oczekiwaniem na gracza
         document.getElementById("root").classList = ""; //usuwa klasę login-shade(anuluje przyciemnienie)
-        document.getElementById("status").innerHTML = `Witaj&nbsp<span>${
+        document.getElementById("status").innerHTML = `<div>Witaj&nbsp<span>${
           res.nickname
-        }</span>, grasz ${res.player ? "czarnymi" : "białymi"}`; //set status na witaj "gracz" ...
+        }</span>, grasz ${
+          res.player ? "czarnymi" : "białymi"
+        }</div><div id='clock'>30</div>`; //set status na witaj "gracz" ...
         if (res.player != 0) {
           this.turnOnTimer();
           window.onmousedown = () => console.log("wait for your move!");
@@ -37,6 +39,7 @@ class Ui {
         } else {
           this.game.player = "white";
           this.game.allowedToMove = 1;
+          this.turnOnClock();
           window.onmousedown = (e) => this.game.handleClick(e);
         } //raycaster działa po zalogowaniu
       } else {
@@ -56,6 +59,7 @@ class Ui {
       timer.innerText = currTime;
       if (currTime == 0) {
         timer.innerText = "Przeciwnik się nie ruszył wygrałeś";
+        window.onmousedown = function () {};
         clearInterval(this.interval);
       }
     }, 1000);
@@ -72,8 +76,26 @@ class Ui {
       game.rebuild();
     });
   }
+  turnOnClock() {
+    let timer = document.getElementById("clock");
+    timer.innerText = "30";
+    timer.style.visibility = "visible";
+    this.interval = setInterval(() => {
+      let currTime = timer.innerText * 1;
+      currTime -= 1;
+      timer.innerText = currTime;
+      if (currTime == 0) {
+        let lose = document.getElementById("timer");
+        lose.innerText = "Skończył ci się czas, przegrałeś";
+        lose.style.display = "flex";
+        window.onmousedown = function () {};
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
   waitForLogin() {
     document.getElementById("login-form").style.display = "none";
-    document.getElementById("wait").style.display = "block";
+    document.getElementById("wait").style.display = "flex";
   }
 }
